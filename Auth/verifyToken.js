620f32e45f1f79f1fc00fafed2c55bbd218b87a2
sanskar sanas <sanskarsanas4@gmail.com>
2026-01-16 16:49:35 +0530

    added

--- Auth/verifyToken.js
+++ Auth/verifyToken.js
@@ -6,7 +6,7 @@ const verifyToken = {
 
     verifyAccessToken: (req, res, next) => {
         try {
-            const token = req.headers.authorization.split(' ')[1]
+            const token = req.headers.authorization.split(' ')[1]  || req.cookies.accessToken; 
 
             if (!token) {
                 return res.status(400).json({ msg: 'token not found!' })
