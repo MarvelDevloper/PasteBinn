e296dc5c97cd5e6c17f3cd75bf60e4117bf8157b
sanskar sanas <sanskarsanas4@gmail.com>
2026-01-16 17:36:09 +0530

    updated

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express=require('express')
 const DBconnection = require('./dbConnection/DBconnection')
 const { globalErrorHandler } = require('./middleware/ErrorHander')
 const app=express()
+const cors = require('cors');
 require('dotenv').config()
 const apiratelimit=require('./middleware/apiRatelimit')
 const userRoute = require('./route/userRoute')
 const pasteRoute = require('./route/pasteRoute')
 const otpRoute = require('./route/otpRoutes')
-const corsConfig=require('./config/configCors')
+// const corsConfig=require('./config/configCors')
 // DB connection 
 DBconnection()
 
-
-app.use(corsConfig)
+app.set('trust proxy', 1); 
+app.use(cors({
+  origin: ["http://localhost:5173", "https://yourfrontend.onrender.com"],
+  credentials: true
+}));
+// app.use(corsConfig)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(apiratelimit(15*60*1000,100))
